789f42da73be4b2dd64f342a0e811b9d910055e4
Sushant <sushantkhadka2060@gmail.com>
2024-08-18 19:15:18 +0545

    update

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:637872687540:web:18f5544cc5be1ca4a10ae1";
+  static String apiKey = "AIzaSyAx5PnCFkbRnI_Y-NgfBR7qTUoL1RELEFs";
+  static String messagingSenderId = "637872687540";
+  static String projectId = "teamchat-2038f";
+}
